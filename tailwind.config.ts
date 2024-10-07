900f1f967203ec6cc676c73c2af4ddc459f500a6
Royweru <royezi17@gmail.com>
2024-10-07 19:20:52 +0300

    deleted:    .env 	modified:   .gitignore 	modified:   app/globals.css 	modified:   app/page.tsx 	modified:   tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,10 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		fontFamily:{
+			poppins:'var(--font-poppins)'
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
